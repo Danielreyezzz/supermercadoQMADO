49661de2247fd24251702bda7bc48c86cf4af004
Jairo <jgtrue@protonmail.com>
2023-06-01 10:26:02 +0200

    aside nav to integrate

---
+++ js/asidenav.js
@@ -0,0 +1,54 @@
+// TODO ADAPTAR
+
+// const asideNavButton = document.getElementById('asideNavButton');
+// const asideNavText = document.getElementById('asideNavText');
+// const asidecloseIcon = document.getElementById('aside-close');
+// const asideMenu = document.getElementById('asideMenu');
+// const containerPanel = document.getElementById('containerPanel');
+
+
+// function openAsideMenu() {
+//     asideNavButton.classList.replace('bi-list', 'bi-x');
+//     asideNavText.classList.add('border-bottom');
+//     if (asideMenu.classList.contains('fade-out-left')) {
+//         asideMenu.classList.replace('fade-out-left', 'fade-in-left');
+//     }
+//     asideMenu.classList.remove('d-none');
+// }
+
+// function closeAsideMenu() {
+//     if (asideNavButton.classList.contains('bi-x')) {
+//         asideNavButton.classList.replace('bi-x', 'bi-list');
+//         asideNavText.classList.remove('border-bottom');
+//         asideMenu.classList.replace('fade-in-left', 'fade-out-left');
+//         setTimeout(function () {
+//             asideMenu.classList.add('d-none');
+//         }, 600);
+//     }
+// }
+
+// containerPanel.addEventListener('click', function () {
+//     closeAsideMenu();
+// });
+// asidecloseIcon.addEventListener('click', function () {
+//     closeAsideMenu();
+// });
+
+// asideNavButton.addEventListener("click", function () {
+
+//     if (asideNavButton.classList.contains('bi-list')) {
+//         openAsideMenu()
+//     } else {
+//         closeAsideMenu()
+//     }
+// });
+// asideNavText.addEventListener("click", function () {
+
+//     if (asideNavButton.classList.contains('bi-list')) {
+//         openAsideMenu()
+//     } else {
+//         closeAsideMenu()
+//     }
+// });
+
+
