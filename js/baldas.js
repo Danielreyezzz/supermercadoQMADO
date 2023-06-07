6d4fef341b567c87c078c18d885f4dbad81e2c59
Danielreyezzz <dangioparefape@gmail.com>
2023-06-07 12:35:30 +0200

    suavidad transiciones

--- js/baldas.js
+++ js/baldas.js
@@ -17,25 +17,47 @@ for (let i = 1; i <= 24; i++) {
 }
 
 function show_eye(slot) {
-    let eye = slot.lastElementChild;
-    let cart = eye.previousElementSibling;
-    if (eye.classList.contains('d-none')) {
-        eye.classList.remove('d-none');
-    }
-    if (cart.classList.contains('d-none')) {
-        cart.classList.remove('d-none');
-    }
+  let eye = slot.lastElementChild;
+  let cart = eye.previousElementSibling;
+
+  eye.style.transition = 'opacity 0.3s ease'; // Transición de 0.3 segundos con un efecto de transición "ease"
+  cart.style.transition = 'opacity 0.3s ease'; // Transición de 0.3 segundos con un efecto de transición "ease"
+  eye.style.opacity = '1'; // Establecer opacidad a 1
+  cart.style.opacity = '1'; // Establecer opacidad a 1
+
+  eye.classList.remove('d-none');
+  cart.classList.remove('d-none');
 }
 
 function hide_eye(slot) {
-    let eye = slot.lastElementChild;
-    let cart = eye.previousElementSibling;
-    if (!eye.classList.contains('d-none')) {
-        eye.classList.add('d-none');
-    }
-    if (!cart.classList.contains('d-none')) {
-        cart.classList.add('d-none');
-    }
+  let eye = slot.lastElementChild;
+  let cart = eye.previousElementSibling;
+
+  eye.style.transition = 'opacity 0.3s ease'; // Transición de 0.3 segundos con un efecto de transición "ease"
+  cart.style.transition = 'opacity 0.3s ease'; // Transición de 0.3 segundos con un efecto de transición "ease"
+
+  // Agregar evento para ocultar los elementos cuando el ratón sale de ellos
+  slot.addEventListener('mouseleave', function() {
+    eye.style.opacity = '0'; // Reducir gradualmente la opacidad a 0
+    cart.style.opacity = '0'; // Reducir gradualmente la opacidad a 0
+
+    setTimeout(function() {
+      eye.classList.add('d-none');
+      cart.classList.add('d-none');
+    }, 300); // Esperar 300 milisegundos (duración de la transición) antes de ocultar los elementos
+  });
+
+  // Agregar evento para mostrar los elementos cuando el ratón entra en ellos
+  slot.addEventListener('mouseenter', function() {
+    eye.style.opacity = '1'; // Aumentar gradualmente la opacidad a 1
+    cart.style.opacity = '1'; // Aumentar gradualmente la opacidad a 1
+
+    eye.classList.remove('d-none');
+    cart.classList.remove('d-none');
+  });
+
+  eye.classList.remove('d-none');
+  cart.classList.remove('d-none');
 }
 
 // !quitar despues
